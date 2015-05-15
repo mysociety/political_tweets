38abaea3310cd0406965987b9d79504b0e76a0b4
Chris Mytton <chrismytton@gmail.com>
2015-05-15 10:33:19 +0100

    Make resque/redis connection code simpler

--- app.rb
+++ app.rb
@@ -68,9 +68,7 @@ configure do
   set :session_secret, ENV.fetch('SESSION_SECRET')
   set :countries, $countries
 
-  redis_url = ENV["REDISCLOUD_URL"] || ENV["OPENREDIS_URL"] || ENV["REDISGREEN_URL"] || ENV["REDISTOGO_URL"]
-  uri = URI.parse(redis_url)
-  Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Resque.redis = ENV['REDISTOGO_URL']
 end
 
 use OmniAuth::Builder do
