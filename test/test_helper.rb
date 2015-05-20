4ccf0193b2a2da425b7e45e0581c3b32dc35388f
Chris Mytton <chrismytton@gmail.com>
2015-05-20 17:24:04 +0100

    Don't queue resque jobs in tests
    We can test these separately, but having them run in the test is a bit
    confusing.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,12 @@ require 'database_cleaner'
 
 DatabaseCleaner.strategy = :transaction
 
+# We don't want to run resque jobs
+Resque.inline = true
+def Resque.enqueue(*args)
+  true
+end
+
 Sequel.extension :migration
 db = SeePoliticiansTweet::App.database
 Sequel::Migrator.run(db, "db/migrations")
