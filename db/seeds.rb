a83c3fabc3cef7449eac01003fa31d55c8195a9a
Chris Mytton <chrismytton@gmail.com>
2015-05-21 11:24:11 +0100

    Add db/seeds.rb file with some seed data

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+require_relative '../app'
+
+$stderr.puts "Loading database seeds"
+
+database = SeePoliticiansTweet::App.database
+
+user = database[:users].first
+
+if user.nil?
+  user_id = database[:users].insert(
+    id: 1,
+    twitter_uid: '3254010850',
+    token: 'token',
+    secret: 'secret'
+  )
+  user = database[:users].first(id: user_id)
+end
+
+countries = [
+  { name: 'Wales', url: '/wales', github: 'seepoliticianstweet/wales', latest_term_csv: '/wales/term_table/4.csv', user_id: user[:id] },
+  { name: 'France', url: '/france', latest_term_csv: '/france/term_table/7.csv', user_id: user[:id] },
+  { name: 'Spain', url: '/spain', latest_term_csv: '/spain/term_table/7.csv', user_id: user[:id] }
+]
+
+countries.each { |c| database[:countries].insert(c) }
