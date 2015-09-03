23703a5f1b76e87d64e5263834fb0a7271b753cb
Chris Mytton <chrism@mysociety.org>
2015-09-03 18:31:16 +0100

    Add OcdDivsionIdSet for grouping OCD ids
    This allows us to determine the best grouping for a set of OCD division
    ids.

--- test/ocd_division_id_test.rb
+++ test/ocd_division_id_test.rb
@@ -21,3 +21,35 @@ describe OcdDivisionId do
     end
   end
 end
+
+describe OcdDivsionIdSet do
+  it 'picks the common member in the set' do
+    id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
+    id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:wyandotte_county')
+    set = OcdDivsionIdSet.new(id, id2)
+    assert_equal :state, set.common_type
+
+    id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
+    id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
+    id3 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:adair_county')
+    set = OcdDivsionIdSet.new(id, id2, id3)
+    assert_equal :county, set.common_type
+
+    id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
+    id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
+    id3 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:adair_county')
+    id4 = OcdDivisionId.new('ocd-division/country:us/state:ky/boobar:adair_county')
+    set = OcdDivsionIdSet.new(id, id2, id3, id4)
+    assert_equal :state, set.common_type
+
+    ids = %w(
+      ocd-division/country:ht/departement:sud/arrondissement:port-salut/circonscription:1
+      ocd-division/country:ht/departement:sud/arrondissement:port-salut/circonscription:2
+      ocd-division/country:ht/departement:sud/arrondissement:côteaux/circonscription:1
+      ocd-division/country:ht/departement:sud/arrondissement:côteaux/circonscription:2
+      ocd-division/country:ht/departement:nord-est/arrondissement:fort-liberté/circonscription:1
+    ).map { |id| OcdDivisionId.new(id) }
+    set = OcdDivsionIdSet.new(*ids)
+    assert_equal :arrondissement, set.common_type
+  end
+end
