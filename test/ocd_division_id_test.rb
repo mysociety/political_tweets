6c35f04f3748ace5055fe0a5becbc60159394399
Chris Mytton <chrism@mysociety.org>
2015-09-23 16:43:51 +0100

    Group OCD ids by level rather than type
    This means that even if there are different names for a certain level,
    e.g. state and territory in Australia, they will still get grouped at
    the correct level.

--- test/ocd_division_id_test.rb
+++ test/ocd_division_id_test.rb
@@ -11,36 +11,50 @@ describe OcdDivisionId do
     end
   end
 
-  describe '#id_for' do
+  describe '#level' do
     let(:id) { 'ocd-division/country:us/state:ky/county:bourbon_county' }
     subject { OcdDivisionId.new(id) }
-    it 'returns part of the division id' do
-      assert_equal id, subject.id_for(:county)
-      assert_equal 'ocd-division/country:us/state:ky', subject.id_for(:state)
-      assert_equal 'ocd-division/country:us', subject.id_for(:country)
+    it 'returns the requested level of the id' do
+      assert_equal id, subject.level(2)
+      assert_equal 'ocd-division/country:us/state:ky', subject.level(1)
+      assert_equal 'ocd-division/country:us', subject.level(0)
     end
   end
 end
 
 describe OcdDivsionIdSet do
+  describe '#max_level' do
+    it 'returns the max level in all ids' do
+      ids = %w(
+        ocd-division/country:ht/departement:sud/arrondissement:port-salut/circonscription:1
+        ocd-division/country:ht/departement:sud/arrondissement:port-salut
+        ocd-division/country:ht/departement:sud/arrondissement:côteaux/circonscription:1
+        ocd-division/country:ht/departement:sud/arrondissement:côteaux/circonscription:2
+        ocd-division/country:ht/departement:nord-est
+      ).map { |id| OcdDivisionId.new(id) }
+      set = OcdDivsionIdSet.new(*ids)
+      assert_equal 3, set.max_level
+    end
+  end
+
   it 'picks the common member in the set' do
     id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
     id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:wyandotte_county')
     set = OcdDivsionIdSet.new(id, id2)
-    assert_equal :state, set.common_type
+    assert_equal 1, set.common_level
 
     id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
     id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
     id3 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:adair_county')
     set = OcdDivsionIdSet.new(id, id2, id3)
-    assert_equal :county, set.common_type
+    assert_equal 2, set.common_level
 
     id = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
     id2 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:bourbon_county')
     id3 = OcdDivisionId.new('ocd-division/country:us/state:ky/county:adair_county')
     id4 = OcdDivisionId.new('ocd-division/country:us/state:ky/boobar:adair_county')
     set = OcdDivsionIdSet.new(id, id2, id3, id4)
-    assert_equal :state, set.common_type
+    assert_equal 2, set.common_level
 
     ids = %w(
       ocd-division/country:ht/departement:sud/arrondissement:port-salut/circonscription:1
@@ -50,6 +64,6 @@ describe OcdDivsionIdSet do
       ocd-division/country:ht/departement:nord-est/arrondissement:fort-liberté/circonscription:1
     ).map { |id| OcdDivisionId.new(id) }
     set = OcdDivsionIdSet.new(*ids)
-    assert_equal :arrondissement, set.common_type
+    assert_equal 2, set.common_level
   end
 end
