d10e9236eb15b0d5aeacd6f8ecae885c4ba2e604
Chris Mytton <chrism@mysociety.org>
2015-09-25 10:31:23 +0100

    Add status flag to submission model
    This allows us to track pending, approved and rejected submissions.

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,16 @@ module SeePoliticiansTweet
   module Models
     class Submission < Sequel::Model
       many_to_one :site
+
+      def before_validation
+        self.status ||= 'pending'
+        super
+      end
+
+      def validate
+        super
+        validates_includes ['pending', 'approved', 'rejected'], :status
+      end
     end
   end
 end
