18258a8b61e9d2b58620f7b1ef8192736a72e76b
Zarino Zappia <mail@zarino.co.uk>
2015-10-05 16:42:48 +0100

    Enable momentum scrolling on iOS devices
    Fixes #48.

--- jekyll/repo/js/main.js
+++ jekyll/repo/js/main.js
@@ -5,3 +5,13 @@ if (selectedElement) {
   var parentElement = selectedElement.parentNode;
   parentElement.scrollTop = selectedElement.offsetTop - (parentElement.clientHeight / 2) + (selectedElement.clientHeight / 2);
 }
+
+// Detect whether this device hides its scrollbars. If it does,
+// we can use `overflow: scroll` (rather than auto) and enable
+// momentum scrolling for the area list on iOS devices.
+var areaListElement = document.querySelector('.area-list');
+var areaListStyle = window.getComputedStyle(areaListElement);
+var areaListWidth = areaListElement.clientWidth + parseInt(areaListStyle['border-right-width']) + parseInt(areaListStyle['border-left-width']);
+if (areaListWidth == areaListElement.offsetWidth) {
+  document.body.className += ' device-has-invisible-scrollbars';
+}
